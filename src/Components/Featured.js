3705ae3169582f7795cc9900f020836485bf5d7a
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-09-14 10:20:23 +0100

    featured section

--- src/Components/Featured.js
+++ src/Components/Featured.js
@@ -23,4 +23,38 @@ const Featured = () => {
 export default Featured
 
 
-const FeaturedStyled = styled.section``;
+const FeaturedStyled = styled.section`
+    position: relative;
+    &::before {
+        content: '';
+        position: absolute;
+        width: 200px;
+        height: 200px;
+        border-radius: 50%;
+        background: #E5D5FA;
+        top: 0;
+        left: 0;
+        z-index: -1;
+    }
+    .subtitle {
+        text-transform: uppercase;
+        font-weight: bold;
+        color: #6E00FF;
+        letter-spacing: .2em;
+        font-size: .85em;
+    }
+    .featured-title {
+        color: black;
+        font-weight: bold;
+        font-size: 1.3em;
+        margin-top: .4em;
+        display: block;
+    }
+    .featured-desc {
+        color: #252525;
+        font-size: .9em;
+        line-height: 1.8em;
+        font-weight: 500;
+        padding: 1.5rem 0;
+    }
+`;
